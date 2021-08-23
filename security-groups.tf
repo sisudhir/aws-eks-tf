3b303aa9881cbf9c3607693d9cf58acf5d9959ae
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-23 17:36:56 +0530

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -9,8 +9,17 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
+      "10.100.0.0/16",
+      "192.168.1.0/24",
+      "192.168.2.0/24",
     ]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
@@ -24,11 +33,66 @@ resource "aws_security_group" "worker_group_mgmt_two" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "192.168.0.0/16",
+      "10.100.0.0/16",
+      "192.168.1.0/24",
+      "192.168.2.0/24",
     ]
+  }
+    
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_security_group" "worker_group_mgmt_three" {
+  name_prefix = "worker_group_mgmt_three"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.100.0.0/16",
+      "192.168.1.0/24",
+      "192.168.2.0/24",
+    ]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}  
+  
+resource "aws_security_group" "worker_group_mgmt_four" {
+  name_prefix = "worker_group_mgmt_four"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.100.0.0/16",
+      "192.168.1.0/24",
+      "192.168.2.0/24",
+    ]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
@@ -39,9 +103,11 @@ resource "aws_security_group" "all_worker_mgmt" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
+      "10.100.0.0/16",
       "172.16.0.0/12",
       "192.168.0.0/16",
     ]
-  }
-}
+  }    
+}  
+    
+  
